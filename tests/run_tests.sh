94c4d52171e20626ccc14aaf635c84a856cac42c
Will Handley <wh260@cam.ac.uk>
2018-11-22 10:37:08 +0000

    Tests now complete

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -17,6 +17,8 @@ rm -f COM_Likelihood_Data-baseline_R2.00.tar.gz
 make
 
 export OMP_NUM_THREADS=1
+echo "nlive=5" >> test.ini
+echo "max_ndead=5" >> test.ini
 mpirun -np 1 --allow-run-as-root ./cosmomc test.ini
 
 mpirun -np 1 --allow-run-as-root ./cosmomc test_planck.ini
